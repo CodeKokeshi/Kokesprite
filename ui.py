42263aa6de25f5d219d1551359ce748a8affead3
CodeKokeshi <kokeshiaikawa003@gmail.com>
2025-08-21 10:28:20 +0800

    Refactor size control layout and add section labels in UI

--- ui.py
+++ ui.py
@@ -25,15 +25,21 @@ class UI:
     def setup_size_controls(self):
         """Setup size control elements, anchored to current x/y/width/height."""
         controls = {}
-        row2_y = self.y + 35
-        controls['minus'] = pygame.Rect(self.x + 5, row2_y, 25, 25)
-        controls['plus'] = pygame.Rect(self.x + 35, row2_y, 25, 25)
-        controls['shape'] = pygame.Rect(self.x + 70, row2_y, 80, 25)
-        # Pixel Perfect toggle
-        controls['pixel_perfect'] = pygame.Rect(self.x + 160, row2_y, 120, 25)
-        # Size slider and input
-        controls['slider'] = pygame.Rect(self.x + 5, row2_y + 30, 220, 20)
-        controls['input'] = pygame.Rect(self.x + 235, row2_y + 28, 60, 24)
+        
+        # Brush Size section (left side)
+        size_start_x = self.x + 10
+        size_y = self.y + 40  # Raised up a bit
+        controls['minus'] = pygame.Rect(size_start_x, size_y, 25, 25)
+        controls['plus'] = pygame.Rect(size_start_x + 30, size_y, 25, 25)
+        controls['slider'] = pygame.Rect(size_start_x + 60, size_y + 2, 160, 20)
+        controls['input'] = pygame.Rect(size_start_x + 230, size_y, 50, 25)
+        
+        # Brush Options section (right side)
+        options_start_x = self.x + 320
+        options_y = self.y + 40  # Same height as size controls
+        controls['shape'] = pygame.Rect(options_start_x, options_y, 80, 25)
+        controls['pixel_perfect'] = pygame.Rect(options_start_x + 90, options_y, 100, 25)
+        
         return controls
 
     def handle_click(self, mouse_pos, tool_manager, size_input_text, previous_valid_size, pixel_perfect):
@@ -116,15 +122,32 @@ class UI:
         pygame.draw.rect(screen, (240, 240, 240), ui_rect)
         pygame.draw.rect(screen, BLACK, ui_rect, 1)
 
+        # Section labels
+        size_label = self.font.render("Brush Size", True, BLACK)
+        screen.blit(size_label, (self.x + 10, self.y + 10))
+        
+        options_label = self.font.render("Brush Options", True, BLACK)
+        screen.blit(options_label, (self.x + 320, self.y + 10))
+
         # Size controls
         current_tool = tool_manager.get_current_tool()
-        controls_info = [
+        size_controls_info = [
             ('minus', self.size_controls['minus'], "-"),
             ('plus', self.size_controls['plus'], "+"),
+        ]
+        for name, rect, text_content in size_controls_info:
+            pygame.draw.rect(screen, WHITE, rect)
+            pygame.draw.rect(screen, BLACK, rect, 1)
+            text = self.font.render(text_content, True, BLACK)
+            text_rect = text.get_rect(center=rect.center)
+            screen.blit(text, text_rect)
+
+        # Option controls
+        option_controls_info = [
             ('shape', self.size_controls['shape'], current_tool.shape.capitalize()),
             ('pixel_perfect', self.size_controls['pixel_perfect'], "Pixel Perfect"),
         ]
-        for name, rect, text_content in controls_info:
+        for name, rect, text_content in option_controls_info:
             color = (200, 255, 200) if name == 'pixel_perfect' and pixel_perfect else WHITE
             pygame.draw.rect(screen, color, rect)
             pygame.draw.rect(screen, BLACK, rect, 1)
@@ -145,8 +168,6 @@ class UI:
         input_color = (255, 255, 200) if size_input_active else WHITE
         pygame.draw.rect(screen, input_color, input_rect)
         pygame.draw.rect(screen, BLACK, input_rect, 1)
-        size_label = self.font.render("Size:", True, BLACK)
-        screen.blit(size_label, (input_rect.x - 40, input_rect.y + 3))
         display_text = size_input_text if size_input_text else "..."
         text_color = BLACK if size_input_text else GRAY
         input_text_surface = self.font.render(display_text, True, text_color)
